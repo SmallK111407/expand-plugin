1cf756d2c093ccaf246bae038ce04b6c7df566bf
SmallK111407 <3399280843@qq.com>
2023-01-28 07:25:52 +0800

    适配锅巴

---
+++ guoba.support.js
@@ -0,0 +1,44 @@
+import setting from "./models/setting.js";
+import lodash from "lodash";
+
+// 支持锅巴
+export function supportGuoba () {
+    return {
+      pluginInfo: {
+        name: 'expand-plugin',
+        title: '拓展插件',
+        author: '@曉K',
+        authorLink: 'https://gitee.com/SmallK111407',
+        link: 'https://gitee.com/SmallK111407/expand-plugin',
+        isV3: true,
+        isV2: false,
+        description: '提供#刻晴攻略5、#艾尔海森攻略6、#明日素材 等在云崽基础上拓展的功能',
+        icon: 'bi:box-seam',
+        iconColor: '#7ed99e'
+      },
+      // 配置项信息
+    configInfo: {
+        // 配置项 schemas
+        schemas: [{
+          field: 'RecordHelp.enable',
+          label: '适用于TRSS-Plugin的记录帮助',
+          bottomHelpMessage: '是否启用该功能',
+          component: 'Switch'
+        }],
+
+        getConfigData () {
+            return setting.merge()
+          },
+          // 设置配置的方法（前端点确定后调用的方法）
+          setConfigData (data, { Result }) {
+            let config = {}
+            for (let [keyPath, value] of Object.entries(data)) {
+              lodash.set(config, keyPath, value)
+            }
+            config = lodash.merge({}, setting.merge, config)
+            setting.analysis(config)
+            return Result.ok({}, '保存成功~')
+          }
+        }
+      }
+    }
