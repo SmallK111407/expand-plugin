2d47d4499aa17ce1af5393eb3066f331f95dd354
SmallK111407 <3399280843@qq.com>
2023-04-23 18:21:56 +0800

    更新#随机时雨

--- guoba.support.js
+++ guoba.support.js
@@ -2,57 +2,63 @@ import setting from "./models/setting.js";
 import lodash from "lodash";
 
 // 支持锅巴
-export function supportGuoba () {
-    return {
-      pluginInfo: {
-        name: 'expand-plugin',
-        title: '拓展插件',
-        author: '@曉K',
-        authorLink: 'https://gitee.com/SmallK111407',
-        link: 'https://gitee.com/SmallK111407/expand-plugin',
-        isV3: true,
-        isV2: false,
-        description: '提供#刻晴攻略5、#艾尔海森攻略6、#明日素材 等在云崽基础上拓展的功能',
-        icon: 'bi:box-seam',
-        iconColor: '#7ed99e'
-      },
-      // 配置项信息
+export function supportGuoba() {
+  return {
+    pluginInfo: {
+      name: 'expand-plugin',
+      title: '拓展插件',
+      author: '@曉K',
+      authorLink: 'https://gitee.com/SmallK111407',
+      link: 'https://gitee.com/SmallK111407/expand-plugin',
+      isV3: true,
+      isV2: false,
+      description: '提供#刻晴攻略5、#艾尔海森攻略6、#明日素材 等在云崽基础上拓展的功能',
+      icon: 'bi:box-seam',
+      iconColor: '#7ed99e'
+    },
+    // 配置项信息
     configInfo: {
-        // 配置项 schemas
-        schemas: [{
-          field: 'RecordHelp.enable',
-          label: '记录帮助',
-          bottomHelpMessage: '是否覆盖云崽的记录帮助',
-          component: 'Switch'
-        },{
-          field: 'CKorSKHelp.enable',
-          label: 'CK或SK帮助',
-          bottomHelpMessage: '是否覆盖ck或sk帮助',
-          component: 'Switch'
-        },{  
-          field: 'CKorSKHelp.docs',
-          label: 'ck或sk教程链接',
-          bottomHelpMessage: '发送出来的教程链接',
-          component: 'Input',
-          required: true,
-          componentProps: {
+      // 配置项 schemas
+      schemas: [{
+        field: 'RecordHelp.enable',
+        label: '记录帮助',
+        bottomHelpMessage: '是否覆盖云崽的记录帮助',
+        component: 'Switch'
+      }, {
+        field: 'CKorSKHelp.enable',
+        label: 'CK或SK帮助',
+        bottomHelpMessage: '是否覆盖ck或sk帮助',
+        component: 'Switch'
+      }, {
+        field: 'CKorSKHelp.docs',
+        label: 'ck或sk教程链接',
+        bottomHelpMessage: '发送出来的教程链接',
+        component: 'Input',
+        required: true,
+        componentProps: {
           placeholder: '请输入链接',
-        },
-      }],
-
-        getConfigData () {
-            return setting.merge()
-          },
-          // 设置配置的方法（前端点确定后调用的方法）
-          setConfigData (data, { Result }) {
-            let config = {}
-            for (let [keyPath, value] of Object.entries(data)) {
-              lodash.set(config, keyPath, value)
-            }
-            config = lodash.merge({}, setting.merge, config)
-            setting.analysis(config)
-            return Result.ok({}, '保存成功~')
-          }
         }
+      }, {
+        field: 'RandomTRSS.enable',
+        label: '随机时雨图',
+        bottomHelpMessage: '是否启用随机时雨图',
+        component: 'Switch'
+      }
+      ],
+
+      getConfigData() {
+        return setting.merge()
+      },
+      // 设置配置的方法（前端点确定后调用的方法）
+      setConfigData(data, { Result }) {
+        let config = {}
+        for (let [keyPath, value] of Object.entries(data)) {
+          lodash.set(config, keyPath, value)
+        }
+        config = lodash.merge({}, setting.merge, config)
+        setting.analysis(config)
+        return Result.ok({}, '保存成功~')
       }
     }
+  }
+}
